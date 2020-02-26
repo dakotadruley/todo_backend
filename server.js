b6ae165122a6b0bb025be046471e85b3d28658ff
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-02-26 11:34:50 -0800

    this

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.post('/api/todos', async (req, res) => {
 
         const result = await client.query(`
         insert into todos (task, complete)
-        values ($1, false)
+        values ($1, $2)
         returning *;`,
 
         [req.body.task, req.body.complete]);
